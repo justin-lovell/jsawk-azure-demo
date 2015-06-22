039bc3d019c4f49bd934086ac818cf06987da47f
Justin Lovell <justin@justjuzzy.com>
2015-06-22 22:59:58 +0200

    setting up the environment for jsawk

---
+++ _conf-env.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#OBTAIN spider-monkey
+sudo apt-get install mercurial autoconf2.13 –y
+hg clone http://hg.mozilla.org/mozilla-central/ 
+cd mozilla-central/js/src
+autoconf2.13
+./configure
+make
+sudo make install
+
+# OBTAIN jsawk
+
+sudo apt-get install curl -y
+curl -L http://github.com/micha/jsawk/raw/master/jsawk > jsawk
+sudo chmod 755 jsawk
+touch ~/.jsawkrc
+sudo mv jsawk /usr/bin/
+
+
+
+echo '[]' | jsawk 'return this.length'
+
